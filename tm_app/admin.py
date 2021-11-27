7ccba96003a8e15bd5af10b125fafb95686bbcd9
robertpaulovcak <robert.paulovcak@gmail.com>
2021-11-27 21:44:42 +0100

    running admin correctly

--- tm_app/admin.py
+++ tm_app/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
-from tm_app.models import Person
+from tm_app.models import Person, Task
 
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = [
-        'name'
+        'name',
+        'id'
+    ]
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        'task_name',
+        'person',
+        'status',
+
     ]
 
 admin.site.register(Person, PersonAdmin)
+admin.site.register(Task, TaskAdmin)
--- tm_app/models.py
+++ tm_app/models.py
@@ -6,13 +6,24 @@ from django.db import models
 #Createyourmodelshere.
 
 class Person(models.Model):
-    name=models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
 
     def __str__(self):
-        return f'{self.name}:{self.id}'
+        return f'{self.name} : {self.id}'
 
 
 class Task(models.Model):
-    task_name=models.CharField(max_length=256)
-    status=models.CharField(max_length=256)
-    created_at=models.DateTimeField(auto_now_add=True)
+    ACTIVE = 'AC'
+    CLOSED = 'CL'
+    STATUS_CHOICES = [
+        (ACTIVE, 'Active'),
+        (CLOSED, 'Closed'),
+    ]
+    task_name = models.CharField(max_length=256)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=2, blank=True, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    person = models.ForeignKey(to='Person', on_delete=models.CASCADE, related_name='task')
+
+
+    def __str__(self):
+        return f'{self.task_name} : {self.person} : {self.status} '
